0ab4116139e9e663c9674b777d1ebc8783d07799
isaac <isaacjulianpavon.alu@iespacomolla.es>
2022-12-08 19:05:39 +0100

    update html and css structure

--- frontend/src/header/header.ts
+++ frontend/src/header/header.ts
@@ -1,33 +1,37 @@
 export function showHiddenHamburger (): void {
   const hamburgerDiv = document.querySelector('div.hamburger') as HTMLDivElement
-  const ulMenuNav = document.querySelector('ul.top-nav-inner') as HTMLUListElement
+  const hamburgerIcon = hamburgerDiv.querySelector('i') as HTMLDialogElement
+  const ulMenuNav = document.querySelector('ul.header-ul') as HTMLUListElement
 
   hamburgerDiv.addEventListener('click', () => {
     let action
-    let icon
+    let icon, iconRemove
     if (ulMenuNav.style.display === 'none') {
       action = 'flex'
-      icon = '<i class="bi bi-x-lg"></i>'
+      icon = 'bi-x-lg'
+      iconRemove = 'bi-list'
     } else {
       action = 'none'
-      icon = '<i class="bi bi-list"></i>'
+      icon = 'bi-list'
+      iconRemove = 'bi-x-lg'
     }
     ulMenuNav.style.display = action
-    hamburgerDiv.innerHTML = icon
+    hamburgerIcon.classList.remove(iconRemove)
+    hamburgerIcon.classList.add(icon)
     console.log(action)
   })
 }
 
 export function loggingUser (): void {
-  const loggingForm = document.querySelector('form#form-logging') as HTMLFormElement
-  const logginLogged = document.querySelector('div.logged') as HTMLDivElement
+  const loggingForm = document.querySelector('form#formLogging') as HTMLFormElement
+  const loggingLogged = document.querySelector('div.logged') as HTMLDivElement
   loggingForm.addEventListener('submit', (event) => {
     event.preventDefault()
 
     const userUser = document.querySelector('#email') as HTMLInputElement
     const userPassword = document.querySelector('#email') as HTMLInputElement
     if (userUser.value === 'usuario' && userPassword.value === 'usuario') {
-      logginLogged.classList.add('ux-show')
+      loggingLogged.classList.add('ux-show')
       loggingForm.classList.add('ux-hidden')
     }
   })
