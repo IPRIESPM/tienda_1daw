1c356385cbc752d4139f8d1f10a78fb0c83581af
isaac <isaacjulianpavon.alu@iespacomolla.es>
2022-12-11 15:56:31 +0100

    create new bracnh, pollicy and responsive contennt

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,6 +1,10 @@
+import { showArticleCard } from './card/card'
+import { deactivatedLinks } from './general/general'
 import { loggingUser, showHiddenHamburger } from './header/header'
 import { showHiddenModal } from './modal/modal'
 
+deactivatedLinks()
+showArticleCard()
 showHiddenHamburger()
 showHiddenModal()
 loggingUser()
